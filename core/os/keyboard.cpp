3ed221f0d3cd352eda090a62eac8aa52633f0eb8
Tom Lechner <tom@tomlechner.com>
2018-04-13 02:03:58 -0700

    Made find_keycode() able to parse modifiers

--- core/os/keyboard.cpp
+++ core/os/keyboard.cpp
@@ -435,12 +435,40 @@ int find_keycode(const String &p_code) {
 
 	const _KeyCodeText *kct = &_keycodes[0];
 
-	while (kct->text) {
+	if (p_code.find("+") < 0) {
 
-		if (p_code.nocasecmp_to(kct->text) == 0) {
-			return kct->code;
+		while (kct->text) {
+
+			if (p_code.nocasecmp_to(kct->text) == 0) {
+				return kct->code;
+			}
+			kct++;
 		}
-		kct++;
+
+	} else {
+		int keycode = 0;
+
+		Vector<String> codes = p_code.split("+");
+
+		for (int i = 0; i < codes.size(); i++) {
+			String code = codes[i];
+			if      (code == find_keycode_name(KEY_SHIFT))   keycode |= KEY_MASK_SHIFT;
+			else if (code == find_keycode_name(KEY_CONTROL)) keycode |= KEY_MASK_CTRL;
+			else if (code == find_keycode_name(KEY_ALT))     keycode |= KEY_MASK_ALT;
+			else if (code == find_keycode_name(KEY_META))    keycode |= KEY_MASK_META;
+			else {
+
+				while (kct->text) {
+
+					if (code.nocasecmp_to(kct->text) == 0) {
+						keycode |= kct->code;
+					}
+					kct++;
+				}
+			}
+		}
+
+		return keycode;
 	}
 
 	return 0;
